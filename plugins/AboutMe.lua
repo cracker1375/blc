28c22b7d925ce26736b6eedf072217aab5e6ee5f
cracker1375 <cracker1375@gmail.com>
2017-01-09 19:37:31 +0330

    Update AboutMe.lua

--- plugins/AboutMe.lua
+++ plugins/AboutMe.lua
@@ -1,14 +1,3 @@
---[[
-
-     **************************
-     *  BlackPlus Plugins...  *
-     *                        *
-     *     By @MehdiHS        *
-     *                        *
-     *  Channel > @Black_Ch   *
-     **************************
-	 
-]]
 do
 
 local function run(msg, matches)
@@ -40,14 +29,3 @@ return {
   run = run
 }
 end
---[[
-
-     **************************
-     *  BlackPlus Plugins...  *
-     *                        *
-     *     By @MehdiHS        *
-     *                        *
-     *  Channel > @Black_Ch   *
-     **************************
-	 
-]]
